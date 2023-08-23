d07b163c0a42ad4a026760633ca6b7873089ecde
kolinco mondol akash <92842628+luchifer2@users.noreply.github.com>
2023-08-23 10:45:29 +0600

    Add files via upload

--- search.c
+++ search.c
@@ -1,3 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node* link;
+} * head;
+
+void createList(int n) {
+    struct node* newnode, * tmp;
+    head = (struct node*)malloc(sizeof(struct node));
+    printf("Enter the data for the first node: ");
+    scanf("%d", &head->data);
+    head->link = NULL;
+    tmp = head;
+
+    for (int i = 2; i <= n; i++) {
+        newnode = (struct node*)malloc(sizeof(struct node));
+        printf("Input data for node %d: ", i);
+        scanf("%d", &newnode->data);
+        newnode->link = NULL;
+        tmp->link = newnode;
+        tmp = tmp->link;
+    }
+}
+
+void printData() {
+    if (head == NULL) {
+        printf("Linked list is empty\n");
+    }
+    else {
+        struct node* ptr = head;
+        while (ptr != NULL) {
+            printf("CURRENT ADDRESS: %p \n DATA: %d \n Next address: %p \n", ptr, ptr->data, ptr->link);
+            ptr = ptr->link;
+        }
+    }
+}
+
+void insertAtBeginning(int data) {
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->data = data;
+    newNode->link = head;
+    head = newNode;
+}
+
+void insertAtEnd(int data) {
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->data = data;
+    newNode->link = NULL;
+
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+
+    struct node* ptr = head;
+    while (ptr->link != NULL) {
+        ptr = ptr->link;
+    }
+    ptr->link = newNode;
+}
+
+void insertAtMiddle(int position, int data) {
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->data = data;
+
+    struct node* ptr = head;
+    for (int i = 1; i < position - 1 && ptr != NULL; i++) {
+        ptr = ptr->link;
+    }
+
+    if (ptr == NULL) {
+        printf("Invalid position\n");
+        return;
+    }
+
+    newNode->link = ptr->link;
+    ptr->link = newNode;
+}
 struct node* linearSearch(int key) {
     struct node* ptr = head;
     while (ptr != NULL) {
@@ -9,13 +89,45 @@ struct node* linearSearch(int key) {
     return NULL;
 }
 
-int searchKey;
+int main() {
+    int n;
+    printf("Input the number of nodes: ");
+    scanf("%d", &n);
+    createList(n);
+    printf("\nData entered in the list:\n");
+    printData();
+
+    int newData;
+
+    printf("Enter data to insert at the beginning: ");
+    scanf("%d", &newData);
+    insertAtBeginning(newData);
+    printf("\nData after inserting at the beginning:\n");
+    printf("Enter data to insert at the end: ");
+    scanf("%d", &newData);
+    insertAtEnd(newData);
+    printf("\nData after inserting at the end:\n");
+
+
+    int position;
+    printf("Enter position to insert at the middle: ");
+    scanf("%d", &position);
+    printf("Enter data to insert at position %d: ", position);
+    scanf("%d", &newData);
+    insertAtMiddle(position, newData);
+    printf("\nData after inserting at the middle:\n");
+    printData();
+    int searchKey;
     printf("Enter a value to search for: ");
+
     scanf("%d", &searchKey);
     struct node* searchResult = linearSearch(searchKey);
     if (searchResult != NULL) {
         printf("Value %d found at address: %p\n", searchKey, searchResult);
     }
     else {
-        printf("Value %d not found in the list.\n", searchKey);
+        printf("Value not found in the list.\n");
     }
+
+    return 0;
+}
